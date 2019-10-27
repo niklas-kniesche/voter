e40145cd78ebeac27f803b7fc387a2639f03d8ef
etowahs <quackmires@gmail.com>
2019-10-27 03:15:39 -0400

    styles from before

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+/*global google*/
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Letter from "./Letter";
 import './App.css';
 import firebase from './firebase.js';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: "",
+      ac: null,
+      shouldHide: true
+    };
+  }
+
+  getAutocomplete = (event) => {
+    console.log("getAutocomplete called");
+    let place = this.autocomplete.getPlace();
+    this.setState({ac: this.autocomplete});
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      // Do anything you like with what was entered in the ac field.
+      console.log('You entered: ' + place.name);
+      return;
+    }
+
+    console.log('You selected: ' + place.formatted_address);
+    this.setState({ address: place.formatted_address });
+  }
+
+  initAutocomplete = (event) => {
+    this.autocomplete = new google.maps.places.Autocomplete(document.getElementById('searchTextField'), 
+      {types: ['address']
+
+    });
+    //this.autocomplete.setTypes(['address']);
+    this.autocomplete.addListener('place_changed', (this.getAutocomplete));
+
+  }
 
   componentDidMount = (event) => {
+    this.initAutocomplete();
     const db = firebase.firestore();
     var docRef = db.collection("voters").doc("voter1");
 
@@ -20,11 +56,59 @@ class App extends Component {
         console.log("Error getting document:", error);
     });
   }
+
+  onChange = (event) => {
+    this.setState({ address: event.target.value });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit called");
+    if (this.state.ac) {
+      let place = this.state.ac.getPlace();
+      console.log("From state" + this.state.addres);
+      console.log("From address" + this.state.addres);
+      if (!place.geometry || place.formatted_address !== this.state.address) {
+        console.log('Bad address ' + place.name);
+        return;
+      } else {
+        console.log("Good address!");
+        let shouldHide = this.state.shouldHide;
+        this.setState({ shouldHide: !shouldHide });
+      }
+    } else {
+      console.log("bad address!");
+    }
+    
+    this.setState({
+      address: "",
+    });
+    console.log("we set the state to stuff");
+  }
+
   render() {
+    const shouldHide = this.state.shouldHide;
     return (
-      <div className='app'>
+      <div>
+
+        <div className="searchHeader" align="center">
+          <h1>test</h1>
+          <p className="lead">Input address which will show you certain things </p>
+          <form onSubmit={this.onSubmit}>
+            <div className="input-group col-sm-8 col-sm-offset-2">
+              <input value={this.state.address} onChange={this.onChange} id="searchTextField" type="text" className="form-control" placeholder="Enter your Connecticut address" aria-label="Recipient's username" aria-describedby="basic-addon2"></input>
+              <div className="input-group-append">
+                <button className="btn btn-outline-secondary" type="submit">Submit</button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div style={{ display: (shouldHide ? 'none' : 'block') }} className="background">
+          
+            <Letter />
+        </div>
       </div>
+
     );
   }
 }
-export default App;
---
+++ src/Letter.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './Letter.css';
+
+export default class Letter extends Component {
+	getDate = (event) => {
+		var today = new Date();
+		var dd = String(today.getDate()).padStart(2, '0');
+		var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+		var yyyy = today.getFullYear();
+
+		today = mm + '/' + dd + '/' + yyyy;
+		return today;
+	}
+
+	render() {
+		return (
+
+			<div className="letter container">
+				<div className="sender">
+					<p>Name</p>
+					<p>Address</p>
+					<p>City, State, Zip Code</p>
+					<p>person@website.com</p>
+				</div>
+
+				<p>{this.getDate()}</p>
+
+				<div className="recipient">
+					<p>Name</p>
+					<p>Address</p>
+					<p>City, State, Zip Code</p>
+					<p>person@website.com</p>
+				</div>
+				<div className="main-text">
+					<p>Dear Registered Voter: </p>
+					<p>What if your neighbors knew if you voted or not?</p>
+					<p>Why do so many people fail to vote? We've been talking about the problem for years, but it only seems to get worse. This year, we're taking a new approach. We're sending this mailing to you and your neighbors to publicize who does and does not vote.</p>
+					<p> The chart shows the names of some of your neighbors, showing which have voted in
+ 						the past. After the August 8 election, we intend to mail an updated chart. You
+ 						and your neighbors will all know who voted and who did not.
+					</p>
+				</div>
+			</div>
+		);
+	}
+}
